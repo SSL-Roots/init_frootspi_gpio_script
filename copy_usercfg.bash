2eebc9703aedf6279f747793cabc5228b734f28b
ShotaAk <macakasit@gmail.com>
2024-08-15 10:05:19 +0900

    Support ubuntu 22 and 24

--- copy_usercfg.bash
+++ copy_usercfg.bash
@@ -1,7 +1,18 @@
 #!/bin/bash -e
 
-cd $(dirname $0)/
-sudo cp /boot/firmware/usercfg.txt /boot/firmware/usercfg-backup.txt
-sudo cp usercfg.txt /boot/firmware/usercfg.txt
+os_version=$(lsb_release -r | awk '{print $2}')
 
+cd $(dirname $0)/
+
+if [[ "$os_version" == "20."* ]]; then
+    echo "Ubuntu 20.04 detected. Updating /boot/firmware/usercfg.txt"
+    sudo cp /boot/firmware/usercfg.txt /boot/firmware/usercfg-backup.txt
+    sudo cp usercfg.txt /boot/firmware/usercfg.txt
+else
+    echo "Ubuntu 22 or 24 detected. Updating /boot/firmware/config.txt"
+    sudo cp /boot/firmware/config.txt /boot/firmware/config-backup.txt
+    sudo cat usercfg.txt | sudo tee -a /boot/firmware/config.txt > /dev/null
+fi
+    
+echo "Update completed."
 echo "設定を反映するため再起動してください"
